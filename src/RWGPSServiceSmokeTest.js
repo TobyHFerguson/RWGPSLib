138dd597564e36a37e2bad5a6f765a7c15c5f024
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-09-28 14:15:31 -0700

    uncommented A_TEMPLATE_URL

--- src/RWGPSServiceSmokeTest.js
+++ src/RWGPSServiceSmokeTest.js
@@ -1,5 +1,5 @@
 // ts-check
-// const A_TEMPLATE_URL = 'https://ridewithgps.com/events/404021-a-template'
+const A_TEMPLATE_URL = 'https://ridewithgps.com/events/404021-a-template'
 
 function getRWGPSService_() {
     const globals = { CANONICAL_EVENT: '' };
