456b937983359358531ad97b8f536f5da3c12305
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-09-28 14:12:55 -0700

    sorted RWGPS by function name

--- src/RWGPS.js
+++ src/RWGPS.js
@@ -2,10 +2,8 @@ function DEBUG_(message) {
   Logger.log(`DEBUG: ${message}`);
 }
 
-function newRWGPSService(globals, credentialManager) {
-  const apiService = new ApiService(credentialManager)
-  const rwgpsService = new RWGPSService(apiService, globals);
-  return rwgpsService;
+function newCredentialManager(scriptProperties) {
+  return new CredentialManager(scriptProperties);
 }
 
 function newRWGPS(rwgpsService) {
@@ -13,9 +11,12 @@ function newRWGPS(rwgpsService) {
   return rwgps;
 }
 
-function newCredentialManager(scriptProperties) {
-  return new CredentialManager(scriptProperties);
+function newRWGPSService(globals, credentialManager) {
+  const apiService = new ApiService(credentialManager)
+  const rwgpsService = new RWGPSService(apiService, globals);
+  return rwgpsService;
 }
+
 /**
  * Class RWGPS represents the services that are obtained by using event urls
  */
@@ -24,6 +25,154 @@ class RWGPS {
     this.globals = rwgpsService.globals;
     this.rwgpsService = rwgpsService;
   }
+
+  /**
+   * 
+   * @param {PublicEventUrl[]} event_urls event urls to be deleted
+   * @returns response from rwgps
+   * @throws Exception if there's an error
+   */
+  batch_delete_events(event_urls) {
+    let event_ids = event_urls.map(e => e.split('/')[4].split('-')[0]);
+    return this.rwgpsService.batch_delete_events(event_ids);
+  }
+
+  /**
+     * 
+     * @param {PublicRouteUrl[]} route_urls route urls to be deleted
+     * @returns response from rwgps
+     * @throws Exception if there's an error
+     */
+  batch_delete_routes(route_urls) {
+    return this.rwgpsService.batch_delete_routes(route_urls);
+  }
+
+  /**
+   * Returns the basic url of the event created by coping the given template
+   * @param {string} template_url - url of the RWGPS template to be copied
+   * @returns the url of the copied event ending in digits
+   */
+  copy_template_(template_url) {
+    let response = this.rwgpsService.copy_template_(template_url);
+    const headers = response.getAllHeaders();
+    return headers['Location'].split('-')[0];
+  }
+
+  /**
+   * Edit the scheduled event at the given url to be consistent with the given event object
+   * @param{event_url} string - the scheduled event to be modified
+   * @param{event} event object - the event to be used as the source of the changes
+   */
+  edit_event(event_url, event) {
+    // RWGPS bug that prevents an event from being properly scheduled if all_day is not set.
+    let new_event = { ...event, all_day: "1" }
+    let response = this.rwgpsService.edit_event(event_url, new_event);
+    response = this.rwgpsService.edit_event(event_url, event);
+    if (response.getResponseCode() !== 200) {
+      throw Error(`received a code ${response.getResponseCode()} when editing event ${event_url}`);
+    }
+    return response;
+  }
+
+  /**
+   * Edit the events as defined by the list of eventEditObjects
+   * @param {EventEditObject[]} eventEditObjects 
+   * @returns the resulting events
+   */
+  edit_events(eventEditObjects) {
+    // These next two lines are to work around an RWGPS bug
+    const eeos = eventEditObjects.map(({ event, url }) => { return { event: { ...event, all_day: "1" }, url } });
+    this.edit_events_(eeos);
+
+    const events = this.edit_events_(eventEditObjects);
+    return events;
+  }
+
+  edit_events_(eventEditObjects) {
+    const responses = this.rwgpsService.edit_events(eventEditObjects);
+    const contents = responses.map(r => r.getContentText())
+    const events = contents.map(c => JSON.parse(c))
+    return events;
+  }
+
+  /**
+   * Get the event at the URL
+   * @param{string} event_url
+   * @returns{object} the event object
+   */
+  get_event(event_url) {
+    let response = this.rwgpsService.getAll([event_url]);
+    return JSON.parse(response[0].getContentText())["event"];
+  }
+
+  /**
+   * Get the events at the given URLs. 
+   * @param{string[]} event_urls
+   * @return{Event[]} events at the given urls
+   * 
+   * For each url that results in an error the event will be undefined.
+   */
+  get_events(event_urls) {
+    const responses = this.rwgpsService.getAll(event_urls);
+    const events = responses.map((r, i) => {
+      const text = r.getContentText();
+      const body = JSON.parse(r.getContentText());
+      if (r.getResponseCode() !== 200) {
+        console.log(`RWGPS.get_events: Error (${r.getResponseCode()}) getting ${event_urls[i]}: ${text}`);
+      }
+      const event = body["event"];
+      return event;
+    });
+    return events;
+  }
+
+  /**
+   * The Organizer (i.e. ride leader) of an event
+   * @typedef Organizer 
+   * @property {string} id the organizer id
+   * @property {string} text the organizer's name
+   */
+  /**
+     * 
+     * @param {string[]} names - list of ride leader names
+     * @param {RWGPS} rwgps - rwgps object to lookup organizers with
+     * @returns {Organizer[]} one or more organizer objects
+     */
+  getOrganizers(names) {
+    //TODO - use the private table mechanism to get all users in one go.
+    if (!names) return [];
+    //convert the names into the organizer structure
+    const organizers = names.map(name => this.lookupOrganizer(this.globals.A_TEMPLATE, name.trim()));
+    //Figure out if any of the names are known
+    const knownOrganizers = organizers.filter(o => o.id !== this.globals.RIDE_LEADER_TBD_ID)
+    //If any names are known then return them, else return the TBD organizer
+    return (knownOrganizers.length ? knownOrganizers : { id: this.globals.RIDE_LEADER_TBD_ID, text: this.globals.RIDE_LEADER_TBD_NAME });
+  }
+
+  /**
+   * Return the object at the given route url
+   * @param {string} route_url - url of route to be fetched
+   */
+  getRouteObject(route_url) {
+    const response = this.rwgpsService.getRoute(route_url);
+    const o = JSON.parse(response.getContentText());
+    return o;
+  }
+
+  /**
+   * Return the counts for each of the given event urls.
+   * A count of 0 is given for any url that throws an error, and a log message is recorded.
+   * @param {PublicEventUrl[]} event_urls
+   * @param{string[]} rideLeaders - array of array of ride leader names, corresponding to the event_urls
+   * @returns{Number[]} the counts, in the same order as the corresponding event url.
+   */
+  getRSVPCounts(event_urls, rideLeaders) {
+    if (!event_urls || event_urls.length === 0) {
+      return [0]
+    }
+    return event_urls.map(u => this.getRSVPObjectByURL(u).participants.length)
+  }
+
   /**
    * get the RSVP Object given an event id
    * @param {number} event_id the id of the event
@@ -46,7 +195,7 @@ class RWGPS {
       return { name: `No such event: ${event_id}`, participants: [] }
     }
   }
-  
+
   /**
    * Get the RSVP Object that corresponds to the event at the given url
    * @param {PublicEventUrl} e_url Url of an event
@@ -137,175 +286,38 @@ class RWGPS {
     }
   }
 
-
   /**
-   * Return the counts for each of the given event urls.
-   * A count of 0 is given for any url that throws an error, and a log message is recorded.
-   * @param {PublicEventUrl[]} event_urls
-   * @param{string[]} rideLeaders - array of array of ride leader names, corresponding to the event_urls
-   * @returns{Number[]} the counts, in the same order as the corresponding event url.
+   * Import a route from a foreign route URL into the club library
+   * @param {Route} route - the foreign route url or object
+   * @returns {PublicRouteUrl} url - the url of the new route in the club library
+   * @throws Exception if the import fails for any reason.
    */
-  getRSVPCounts(event_urls, rideLeaders) {
-    if (!event_urls || event_urls.length === 0) {
-      return [0]
+  importRoute(route) {
+    let fr;
+    if (typeof route === typeof "") {
+      fr = { url: route }
+    } else {
+      fr = route;
     }
-    return event_urls.map(u => this.getRSVPObjectByURL(u).participants.length)
-  }
 
-  /**
-   * Returns the basic url of the event created by coping the given template
-   * @param {string} template_url - url of the RWGPS template to be copied
-   * @returns the url of the copied event ending in digits
-   */
-  copy_template_(template_url) {
-    let response = this.rwgpsService.copy_template_(template_url);
-    const headers = response.getAllHeaders();
-    return headers['Location'].split('-')[0];
-  }
-  /**
-   * Get the event at the URL
-   * @param{string} event_url
-   * @returns{object} the event object
-   */
-  get_event(event_url) {
-    let response = this.rwgpsService.getAll([event_url]);
-    return JSON.parse(response[0].getContentText())["event"];
-  }
-
-  /**
-   * Get the events at the given URLs. 
-   * @param{string[]} event_urls
-   * @return{Event[]} events at the given urls
-   * 
-   * For each url that results in an error the event will be undefined.
-   */
-  get_events(event_urls) {
-    const responses = this.rwgpsService.getAll(event_urls);
-    const events = responses.map((r, i) => {
-      const text = r.getContentText();
-      const body = JSON.parse(r.getContentText());
-      if (r.getResponseCode() !== 200) {
-        console.log(`RWGPS.get_events: Error (${r.getResponseCode()}) getting ${event_urls[i]}: ${text}`);
-      }
-      const event = body["event"];
-      return event;
-    });
-    return events;
-  }
-  /**
-   * Edit the scheduled event at the given url to be consistent with the given event object
-   * @param{event_url} string - the scheduled event to be modified
-   * @param{event} event object - the event to be used as the source of the changes
-   */
-  edit_event(event_url, event) {
-    // RWGPS bug that prevents an event from being properly scheduled if all_day is not set.
-    let new_event = { ...event, all_day: "1" }
-    let response = this.rwgpsService.edit_event(event_url, new_event);
-    response = this.rwgpsService.edit_event(event_url, event);
-    if (response.getResponseCode() !== 200) {
-      throw Error(`received a code ${response.getResponseCode()} when editing event ${event_url}`);
+    const response = this.rwgpsService.importRoute(fr);
+    const body = JSON.parse(response.getContentText());
+    if (body.success) {
+      let localRoute = { ...fr, url: body.url }
+      this.setRouteExpiration(localRoute);
     }
-    return response;
-  }
-
-  
-
-  /**
-   * Edit the events as defined by the list of eventEditObjects
-   * @param {EventEditObject[]} eventEditObjects 
-   * @returns the resulting events
-   */
-  edit_events(eventEditObjects) {
-    // These next two lines are to work around an RWGPS bug
-    const eeos = eventEditObjects.map(({ event, url }) => { return { event: { ...event, all_day: "1" }, url } });
-    this.edit_events_(eeos);
-
-    const events = this.edit_events_(eventEditObjects);
-    return events;
-  }
-  edit_events_(eventEditObjects) {
-    const responses = this.rwgpsService.edit_events(eventEditObjects);
-    const contents = responses.map(r => r.getContentText())
-    const events = contents.map(c => JSON.parse(c))
-    return events;
-  }
-  /**
-   * 
-   * @param {PublicEventUrl[]} event_urls event urls to be deleted
-   * @returns response from rwgps
-   * @throws Exception if there's an error
-   */
-  batch_delete_events(event_urls) {
-    let event_ids = event_urls.map(e => e.split('/')[4].split('-')[0]);
-    return this.rwgpsService.batch_delete_events(event_ids);
-  }
-  /**
-     * 
-     * @param {PublicRouteUrl[]} route_urls route urls to be deleted
-     * @returns response from rwgps
-     * @throws Exception if there's an error
-     */
-  batch_delete_routes(route_urls) {
-    return this.rwgpsService.batch_delete_routes(route_urls);
+    return body.url;
   }
 
   /**
-   * Add the tags to the events. Both arrays must be non-empty. Idempotent.
-   * @param {PublicEventUrl[]} event_urls - the array of event urls
-   * @param {string[]} tags - the tags
-   * @throws Exception if either array is empty
-   * @returns {HTTPResponse} the response from rwgpsService
-   */
-  tagEvents(event_urls, tags) {
-    if (!event_urls || event_urls.length === 0) {
-      throw new Error('No event URLs given');
-    }
-    if (!tags || tags.length === 0) {
-      throw new Error('No tags given');
-    } 
-    let event_ids = event_urls.map(e => e.split('/')[4].split('-')[0]);
-    return this.rwgpsService.tagEvents(event_ids, tags);
-  }
-  /**
-   * Remove the tags from the events. Both arrays must be non-empty. Idempotent.
-   * @param {PublicEventUrl[]} event_urls - the event urls
-   * @param {string[]} tags - the tags
-   * @throws Exception if either array is empty
-   * @returns {HTTPResponse} the response from rwgpsService
-   */
-  unTagEvents(event_urls, tags) {
-    if (!event_urls || event_urls.length === 0) {
-      throw new Error('No event URLs given');
-    }
-    if (!tags || tags.length === 0) {
-      throw new Error('No tags given');
-    } 
-    let event_ids = event_urls.map(e => e.split('/')[4].split('-')[0]);
-    return this.rwgpsService.unTagEvents(event_ids, tags);
+ * Determine if named ride leader is known
+ * @param {string} name first and last name of ride leader whose status is to be determined
+ * @return {boolean} true iff the ride leader is not the default
+ */
+  knownRideLeader(name) {
+    return this.lookupOrganizer(this.globals.A_TEMPLATE, name).id !== this.globals.RIDE_LEADER_TBD_ID;
   }
 
-  /**
-   * The Organizer (i.e. ride leader) of an event
-   * @typedef Organizer 
-   * @property {string} id the organizer id
-   * @property {string} text the organizer's name
-   */
-  /**
-     * 
-     * @param {string[]} names - list of ride leader names
-     * @param {RWGPS} rwgps - rwgps object to lookup organizers with
-     * @returns {Organizer[]} one or more organizer objects
-     */
-  getOrganizers(names) {
-    //TODO - use the private table mechanism to get all users in one go.
-    if (!names) return [];
-    //convert the names into the organizer structure
-    const organizers = names.map(name => this.lookupOrganizer(this.globals.A_TEMPLATE, name.trim()));
-    //Figure out if any of the names are known
-    const knownOrganizers = organizers.filter(o => o.id !== this.globals.RIDE_LEADER_TBD_ID)
-    //If any names are known then return them, else return the TBD organizer
-    return (knownOrganizers.length ? knownOrganizers : { id: this.globals.RIDE_LEADER_TBD_ID, text: this.globals.RIDE_LEADER_TBD_NAME });
-  }
   /**
    * lookup the organizer id given an event url and the organizer name
    * @param{url} string - the event url
@@ -337,48 +349,6 @@ class RWGPS {
       }
     }
     return TBD;
-  }
-
-  /**
- * Determine if named ride leader is known
- * @param {string} name first and last name of ride leader whose status is to be determined
- * @return {boolean} true iff the ride leader is not the default
- */
-  knownRideLeader(name) {
-    return this.lookupOrganizer(this.globals.A_TEMPLATE, name).id !== this.globals.RIDE_LEADER_TBD_ID;
-  }
-
-  
-  /**
-   * Import a route from a foreign route URL into the club library
-   * @param {Route} route - the foreign route url or object
-   * @returns {PublicRouteUrl} url - the url of the new route in the club library
-   * @throws Exception if the import fails for any reason.
-   */
-  importRoute(route) {
-    let fr;
-    if (typeof route === typeof "") {
-      fr = { url: route }
-    } else {
-      fr = route;
-    }
-
-    const response = this.rwgpsService.importRoute(fr);
-    const body = JSON.parse(response.getContentText());
-    if (body.success) {
-      let localRoute = { ...fr, url: body.url }
-      this.setRouteExpiration(localRoute);
-    }
-    return body.url;
-  }
-  /**
-   * Return the object at the given route url
-   * @param {string} route_url - url of route to be fetched
-   */
-  getRouteObject(route_url) {
-    const response = this.rwgpsService.getRoute(route_url);
-    const o = JSON.parse(response.getContentText());
-    return o;
   }
 
   /**
@@ -439,13 +409,43 @@ class RWGPS {
     }
     return this;
   }
+
+  /**
+   * Add the tags to the events. Both arrays must be non-empty. Idempotent.
+   * @param {PublicEventUrl[]} event_urls - the array of event urls
+   * @param {string[]} tags - the tags
+   * @throws Exception if either array is empty
+   * @returns {HTTPResponse} the response from rwgpsService
+   */
+  tagEvents(event_urls, tags) {
+    if (!event_urls || event_urls.length === 0) {
+      throw new Error('No event URLs given');
+    }
+    if (!tags || tags.length === 0) {
+      throw new Error('No tags given');
+    } 
+    let event_ids = event_urls.map(e => e.split('/')[4].split('-')[0]);
+    return this.rwgpsService.tagEvents(event_ids, tags);
+  }
+
+  /**
+   * Remove the tags from the events. Both arrays must be non-empty. Idempotent.
+   * @param {PublicEventUrl[]} event_urls - the event urls
+   * @param {string[]} tags - the tags
+   * @throws Exception if either array is empty
+   * @returns {HTTPResponse} the response from rwgpsService
+   */
+  unTagEvents(event_urls, tags) {
+    if (!event_urls || event_urls.length === 0) {
+      throw new Error('No event URLs given');
+    }
+    if (!tags || tags.length === 0) {
+      throw new Error('No tags given');
+    } 
+    let event_ids = event_urls.map(e => e.split('/')[4].split('-')[0]);
+    return this.rwgpsService.unTagEvents(event_ids, tags);
+  }
 }
-
-
-
-
-
-
 
 if (typeof module !== 'undefined') {
   module.exports = { RWGPS, RWGPSService }
@@ -454,4 +454,3 @@ if (typeof module !== 'undefined') {
 if (typeof module !== 'undefined') {
   module.exports = RWGPS;
 }
-
